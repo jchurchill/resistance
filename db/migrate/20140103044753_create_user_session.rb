8acde1c7dfc7160bb3d01e754bc7b47b29235fea
jchurchill <justin.e.churchill@gmail.com>
2014-01-07 01:51:58 -0500

    Beautified formatting in migrate file

--- db/migrate/20140103044753_create_user_session.rb
+++ db/migrate/20140103044753_create_user_session.rb
@@ -1,8 +1,8 @@
 class CreateUserSession < ActiveRecord::Migration
   def change
   	create_table :user_sessions do |t|
-      t.string :session_id, :null => false
-      t.text :data
+      t.string 		:session_id, :null => false
+      t.text 			:data
       t.timestamps
     end
 
