dfabf193b9b56839d1b0d35dc1b37d47de44b1c9
jchurchill <justin.e.churchill@gmail.com>
2014-06-15 23:08:10 -0400

    Position absolute on panels

--- app/assets/javascripts/widgets/panel.js
+++ app/assets/javascripts/widgets/panel.js
@@ -1,17 +1,21 @@
 $.widget(
 'jj.panel',
 $.extend(JJ.WidgetBase, (function() {
-	var DUMMY = null;
+	var PANEL_ELEMENT = "panelElement";
 
 	return {
 		_create: function() {
+			// Setup the panel element for future use
+			this.set(PANEL_ELEMENT, this.element.find(".window"));
+			// Make the widget element position absolute so that it can be moved around
+			this.element.addClass("panel-top-level");
 			// Make the close button hide the panel
-			this.element.find(".close").click(function() { this.show(false); }.bind(this))
+			this.get(PANEL_ELEMENT).find(".close").click(function() { this.show(false); }.bind(this))
 		},
 
 		show: function(doShow) {
 			doShow = (typeof doShow !== 'undefined') ? doShow : true;
-			this.element.find(".window").toggleClass("hidden", !doShow);
+			this.get(PANEL_ELEMENT).toggleClass("hidden", !doShow);
 		}
 	};
 })()));
